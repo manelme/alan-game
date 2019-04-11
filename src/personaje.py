7a48c39d286278d2a7c2fd75ce49fc7e8d063a6f
Manel Mena <manel.mena@ual.es>
2019-04-11 09:51:51 +0200

    personaje modificado

--- src/personaje.py
+++ src/personaje.py
@@ -2,11 +2,12 @@
 
 class Personaje():
     
-    def __init__(self, nombre, vida, vida_restante, armadura):
+    def __init__(self, nombre, vida_total, vida_restante, armadura):
         self.nombre = nombre
         self.nivel = 0
         self.porcentaje_de_nivel = 0
         self.elo = 0
-        self.vida = vida
+        self.vida_total = vida_total
         self.vida_restante = vida_restante
         self.armadura = armadura
+    
